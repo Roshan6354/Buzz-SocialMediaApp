9141fba50deaf26a6df3dc35c242ac30a7b97ef8
Roshan <chaudharyroshanrc.186@gamil.com>
2024-07-02 14:21:21 +0530

    routes and controllers for post

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -56,12 +56,14 @@ export const followUnfollowUser = async(req,res) => {
 
         const isFollowing = currentUser.following.includes(id);
         if(isFollowing) {
+            // unfllow
             await User.findByIdAndUpdate(id,{$pull: {followers: req.user._id}});
             await User.findByIdAndUpdate(req.user._id,{$pull: {following: id}});
             res.status(200).json({message: "unfollowed successfully"})
 
         }
         else {
+            // follow
             await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
             await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
 
@@ -161,3 +163,4 @@ export const updateUser = async(req,res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
